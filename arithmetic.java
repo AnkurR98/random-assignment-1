8c8da93602683338c131cd56f0b38387e5b150e0
Ankur Ramaswamy <37382230+AnkurR98@users.noreply.github.com>
2019-03-16 18:14:26 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+class arithmetic    {
+    public static void main(String args[])throws IOException    {
+        String productions_raw[] = new String[3];
+        productions_raw[0] = "E -> T + E | T";
+        productions_raw[1] = "T -> T * F | F";
+        productions_raw[2] = "F -> ( E ) | id";
+        String productions_lfrr[];
+        // productions_lfrr = removeLeftFactoring(productions_raw); {Not to be used atm}
+        productions_lfrr = removeLeftRecursion(productions_raw);
+        for(String debugs : productions_lfrr)   {
+            System.out.println(debugs);
+        }
+        // String variables[] = getVariables(productions_lfrr);
+        // String terminals[] = getTerminals(productions_lfrr);
+    }
+
+    // Under Developement
+    //protected static String removeLeftFactoring(String[] productions_raw)  {
+
+    // }
+
+    protected static String[] removeLeftRecursion(String productions_raw[])   {
+        String productions_mod[] = new String[4];
+        int k = 0;
+        for(String production : productions_raw)    {
+            char a = production.charAt(0);
+            int b = 1, c = 1;
+            while(production.charAt(b)!='>')
+                b++;
+            while(production.charAt(c)!='|')
+                c++;
+            if(a == production.charAt(b+2)) {
+                //Left Recursion occured
+                productions_mod[k] = a + " -> ";
+                productions_mod[k+1] = a + "\' -> ";
+                // String inpString[][] = segregate(production, a);
+                // for(int i = 0; i<inpstring[0].length; i++)
+                //     productions_mod[k] = productions_mod[k] + inpstring[0][i] + " " + a + "\' " + "|";
+                // for(int i = 0; i<inpstring[1].length; i++)
+                //     productions_mod[k+1] = productions_mod[k+1] + inpstring[1][i] + " " + a + "\' " + "|";
+                // productions_mod[k+1] = productions_mod[k+1] + " ε ";
+                // k+=2;
+                productions_mod[k] = productions_mod[k] + production.substring(c+1)  + " "  + a + "\'";
+                productions_mod[k+1] = productions_mod[k+1] + production.substring(b+3,c-1)  + " "  + a + "\'";
+                productions_mod[k+1] = productions_mod[k+1] + " | ε";
+                k+=2;
+            }
+            else{
+                productions_mod[k++] = production;
+            }
+        }
+        return productions_mod;
+    }
+
+    protected static static String[] 
+
+
+}
