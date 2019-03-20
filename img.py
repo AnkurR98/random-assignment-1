c338c20fdc957dd79af20d09eefe420ec2a352fa
Ankur Ramaswamy <37382230+AnkurR98@users.noreply.github.com>
2019-03-20 08:04:41 +0530

    Update img.py

--- img.py
+++ img.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 20 01:46:07 2019
-
-@author: ankur
-"""
-
 import cv2
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 
-img = cv2.imread('./sampleimage.jpg')
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "Your image file here")
+args = vars(ap.parse_args())
+
+img = cv2.imread(args["image"])
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-sensitivity = 16
+sensitivity = 17
 lower_red = np.array([60 - sensitivity,100,0])
 upper_red = np.array([60 + sensitivity,255,255])
 
@@ -27,6 +25,7 @@ cv2.imshow('ORIGINAL IMAGE',img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 
-bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-plt.imshow(res)
+bgr = res
+#bgr = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+plt.imshow(bgr)
 plt.show()
