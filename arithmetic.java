76a3f3e7ce6b836776759d473df6e09e7b29b3d2
Ankur Ramaswamy <ankur.becs.16@acharya.ac.in>
2019-03-21 22:41:21 +0530

    Get variable and terminals added.

--- arithmetic.java
+++ arithmetic.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 class arithmetic    {
     public static void main(String args[])throws IOException    {
@@ -12,8 +13,24 @@ class arithmetic    {
         for(String debugs : productions_lfrr)   {
             System.out.println(debugs);
         }
-        // String variables[] = getVariables(productions_lfrr);
-        // String terminals[] = getTerminals(productions_lfrr);
+        System.out.print("Variables : ");
+        String variables[] = getVariables(productions_lfrr);
+        for(String debugs : variables)   {
+            System.out.print(debugs+"\t");
+        }
+        String terminals[] = getTerminals(productions_lfrr);
+        System.out.print("Terminals : ");
+        for(String debugs : terminals)   {
+            System.out.print(debugs+"\t");
+        }
+    }
+
+    protected static boolean contains(String a, String b[]) {
+        List<String> lst = Arrays.asList(b);
+        if(lst.contains(a))
+            return true;
+        else
+            return false;
     }
 
     // Under Developement
@@ -32,9 +49,9 @@ class arithmetic    {
             while(production.charAt(c)!='|')
                 c++;
             if(a == production.charAt(b+2)) {
-                //Left Recursion occured
                 productions_mod[k] = a + " -> ";
                 productions_mod[k+1] = a + "\' -> ";
+                // Following code is for general grammars, I guess. Not required for current grammar
                 // String inpString[][] = segregate(production, a);
                 // for(int i = 0; i<inpstring[0].length; i++)
                 //     productions_mod[k] = productions_mod[k] + inpstring[0][i] + " " + a + "\' " + "|";
@@ -54,8 +71,38 @@ class arithmetic    {
         return productions_mod;
     }
 
-    // protected static String[] getVariables(String productions_raw[])    {
-    // }
+    protected static String[] getVariables(String productions_lfrr[])    {
+        String s1 = "", variables[] = new String[4];
+        int k = 0, i = 0;
+        for(String production : productions_lfrr)   {
+            while(production.charAt(k) != 32)  {
+                s1 = s1 + (production.charAt(k));
+                // System.out.println(s1);
+                k++;
+            }
+            variables[i++] = s1;
+            k = 0;
+            s1 = "";
+        }
+        return variables;
+    }
 
+    protected static String[] getTerminals(String productions_lfrr[])   {
+        String s1 = "", variables[] = new String[4];
+        int k = 0, i = 0;
+        for(String production : productions_lfrr)   {
+            for(char term : production.toCharArray())  {
+                while(term!=32 || term!='|' || term!='-' || term!='>')    {
+                    s1 = s1 + term;
+                }
+                // System.out.println(s1);
+                
+            }
+            variables[i++] = s1;
+            k = 0;
+            s1 = "";
+        }
+        return variables;
+    }
 
 }
